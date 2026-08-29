708cac15ee8bf33b92c5148418677e61e61aba51
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-08-29 01:19:25 -0400

    throw everything

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,8 +6,10 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm pavucontrol-qt qt6ct kvantum lxqt-qtplugin breeze plasma-integration
+pacman -Syu --noconfirm pavucontrol-qt kvantum lxqt-qtplugin breeze plasma-integration oxygen
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
+
+make-aur-package qt6ct-kde
