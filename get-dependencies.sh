aae0e1f98ad1c27c40bcecd1b720f80530ca29c7
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-08-29 01:08:20 -0400

    test

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm pavucontrol-qt qt6ct kvantum lxqt-qtplugin breeze 
+pacman -Syu --noconfirm pavucontrol-qt qt6ct kvantum lxqt-qtplugin breeze plasma-integration
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
